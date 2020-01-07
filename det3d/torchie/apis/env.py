6c74a0e4e844c7a0601ae49d8fec0ba9e8b12f78
PDC <poodarchu@gmail.com>
2020-01-07 17:10:09 +0800

    Revert "use local_rank instead of rank % num_gpus"

--- det3d/torchie/apis/env.py
+++ det3d/torchie/apis/env.py
@@ -24,7 +24,10 @@ def init_dist(launcher, backend="nccl", **kwargs):
 
 
 def _init_dist_pytorch(backend, **kwargs):
-    torch.cuda.set_device(int(os.environ["LOCAL_RANK"]))
+    # TODO: use local_rank instead of rank % num_gpus
+    rank = int(os.environ["RANK"])
+    num_gpus = torch.cuda.device_count()
+    torch.cuda.set_device(rank % num_gpus)
     dist.init_process_group(backend=backend, **kwargs)
 
 
